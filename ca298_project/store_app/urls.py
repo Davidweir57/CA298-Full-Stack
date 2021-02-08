99acc28d774f8865c334b8ea053e548378cbde97
Gary <murphy.gary18@gmail.com>
2021-02-08 22:59:09 +0000

    model views and form added

--- ca298_project/store_app/urls.py
+++ ca298_project/store_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
 	path('', views.index, name="index"),
 	path('registration/', views.register, name="register"),
+	path('allproducts/', views.all_products, name='all_products'),
+	path('product/<int:prodid>', views.singleproduct, name='single_product'),
+	path('myform', views.myform)
+
 ]
--- ca298_project/store_app/views.py
+++ ca298_project/store_app/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+from django.shortcuts import get_object_or_404
+from .forms import *
 
 # Create your views here.
+
 
 def index(request):
 	return render(request, 'index.html')
 
+
 def register(request):
 	return HttpResponse("Hello from registration page.")
+
+
+def all_products(request):
+	all_p = Product.objects.all()
+	return render(request, 'all_products.html', {'products' : all_p})
+
+
+def singleproduct(request, prodid):
+	product = get_object_or_404(Product, pk=prodid)
+	return render(request, 'single_product.html', {'product': product})
+
+
+def myform(request):
+	if request.method == 'POST':
+		form = ProductForm(request.POST)
+		if form.is_valid():
+			new_product = form.save()
+			return render(request, 'single_product.html', {'product': new_product})
+	else:
+		form = ProductForm()
+		return render(request, 'form.html', {'form': form})
