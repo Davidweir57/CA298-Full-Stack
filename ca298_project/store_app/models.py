7f20786784ff1150d919c073ddde67e00069f75c
Gary <murphy.gary18@gmail.com>
2021-02-09 00:27:56 +0000

    added Category model

--- ca298_project/store_app/models.py
+++ ca298_project/store_app/models.py
@@ -3,9 +3,16 @@ from django.db import models
 # Create your models here.
 
 
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+
 
