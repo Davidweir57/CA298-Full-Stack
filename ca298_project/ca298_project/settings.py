f336d7990ef0ae29384fa7fa2879740247c1181e
Gary <murphy.gary18@gmail.com>
2021-04-12 17:22:46 +0100

    added node app

--- ca298_project/ca298_project/settings.py
+++ ca298_project/ca298_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'store_app'
 ]
 
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ca298_project.urls'
@@ -138,4 +140,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+ "http://localhost:3000",  # node on port 3000
+ "http://127.0.0.1:3000"  # node on port 3000
+]
 
